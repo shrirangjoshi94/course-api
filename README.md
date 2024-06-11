790e2819c16bbd5b6bcc0618adb01227c96e2b05
Shrirang Joshi <shrirangjoshi94@gmail.com>
2024-06-11 23:49:20 +0530

    1st example complete

---
+++ README.md
@@ -0,0 +1,1 @@
+## https://www.youtube.com/playlist?list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x
--- src/main/java/com/api/CourseApiApp/topic/TopicService.java
+++ src/main/java/com/api/CourseApiApp/topic/TopicService.java
@@ -2,22 +2,45 @@ package com.api.CourseApiApp.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topcis = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "spring framework", "desciption 1122"),
             new Topic("spring", "spring framework", "desciption 2"),
             new Topic("spring", "spring framework", "desciption 3")
-    );
+    ));
 
     public List<Topic> getAllTopics()
     {
-        return topcis;
+        return topics;
     }
 
-    public topic 
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public  void  updateTopic(String id, Topic topic) {
+
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+
+            if(t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+    public  void  delelteTopic(String id) {
+
+        topics.removeIf(t -> t.getId().equals(id));
+    }
 }
